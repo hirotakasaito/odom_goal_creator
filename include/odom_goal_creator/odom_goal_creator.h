daeab1e729734ecd5ec4adc3668ef304f1e13c7a
hirotakasaito <tirohiro20208@gmail.com>
2023-11-17 22:03:35 +0900

    fix

--- include/odom_goal_creator/odom_goal_creator.h
+++ include/odom_goal_creator/odom_goal_creator.h
@@ -18,13 +18,17 @@ private:
     void odom_callback(const nav_msgs::Odometry::ConstPtr &);
     std::vector<double> calc_goal();
     void input_goal();
-    void calc_reached_goal(std::vector<double>);
+    double calc_reached_goal(std::vector<double>);
+    int calc_time_out(double);
 
     bool received_odom = false;
     bool received_goal = false;
     bool reached_goal = false;
     bool base_odom_set = false;
+    bool time_out = false;
     int hz;
+    double max_time;
+    int time_out_count = 0;
     std::vector<double> goal;
 
     ros::Subscriber sub_odom;
